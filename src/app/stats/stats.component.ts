a4d0a5fb963058f3275fe79f04cb2a6a42b58373
phursang <psherpa592@gmail.com>
2020-08-24 14:35:49 +0530

    completed ,version 1

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label} from 'ng2-charts';
+import { Label } from 'ng2-charts';
 
-import {Observable} from 'rxjs'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-stats',
@@ -12,24 +12,30 @@ import {Observable} from 'rxjs'
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
-  public covidData:any;
-  public indiaData:any;
+  public covidData: any;
+  public indiaData: any;
 
-  public currentStateSelected:any;
-  public currentStateData:any;
+  public currentStateSelected: any;
+  public currentStateData: any;
+  public currentStateDistricts: any[] = [];
+
+  public currentDistrictSelected: any;
+  public currentDistrictName: any;
 
   public barChartOptions: ChartOptions = {
     responsive: true,
   };
   public barChartLabels: Label[] = ['total', 'today'];
+  public barChartLabelsDistrict: Label[] = ['total'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[];
-  public barChartState:ChartDataSets[];
+  public barChartState: ChartDataSets[];
+  public barChartDistrict: ChartDataSets[];
 
-  constructor(private app:AppService) { }
+  constructor(private app: AppService) { }
 
   ngOnInit() {
 
@@ -37,26 +43,25 @@ export class StatsComponent implements OnInit {
     this.getCovidData();
   }
 
-  private getAllIndiaData()
-  {
-    this.app.getIndiaData().subscribe((res)=>
-    {
-      console.log(res)
-      this.indiaData=res;
+  private getAllIndiaData() {
+    this.app.getIndiaData().subscribe((res) => {
+      // console.log(res)
+      this.indiaData = res;
 
       this.setBarChartData();
 
       // by defalult set currently selected state to first state in array
-      this.currentStateSelected=res['regionData'][0]['region']
+      this.currentStateSelected = res['regionData'][0]['region']
       this.currentStateData = res['regionData'][0];
-      this.setStateBarChart(this.currentStateData)
+      this.setStateBarChart(this.currentStateData);
+
+      this.getCurrentStateDistricts(this.currentStateSelected)
     })
   }
 
-  private setBarChartData()
-  {
+  private setBarChartData() {
     // set bar chart data of current state
-    this.barChartData= [
+    this.barChartData = [
       { data: [this.indiaData.activeCases, this.indiaData.activeCasesNew], label: 'active' },
       { data: [this.indiaData.deaths, this.indiaData.deathsNew], label: 'deaths' },
       { data: [this.indiaData.recovered, this.indiaData.recoveredNew], label: 'recovered' }
@@ -64,49 +69,96 @@ export class StatsComponent implements OnInit {
 
   }
 
-  private setStateBarChart(currentState)
-  {
+  private setStateBarChart(currentState) {
     this.barChartState = [
-      {data:[currentState.totalInfected,currentState.newInfected],label:'active'},
-      {data:[currentState.deceased,currentState.newDeceased],label:'deaths'},
-      {data:[currentState.recovered,currentState.newRecovered],label:'recovered'}
+      { data: [currentState.totalInfected, currentState.newInfected], label: 'active' },
+      { data: [currentState.deceased, currentState.newDeceased], label: 'deaths' },
+      { data: [currentState.recovered, currentState.newRecovered], label: 'recovered' }
     ]
   }
 
-  public getCovidData()
-  {
+  public getCovidData() {
 
     // subscribing to returned observable by http client for data's
-    this.app.getCovidDatas().subscribe((res)=>
-    {
-      console.log(res);
-      this.covidData=res
+    this.app.getCovidDatas().subscribe((res) => {
+      // console.log(res);
+      this.covidData = res
     },
-    (err)=>
-    {
-      console.log(err)
-    })
+      (err) => {
+        console.log(err)
+      })
   }
 
   // fired when state option is selected
-  public stateSelected()
-  {
-    console.log(this.currentStateSelected);
-    if(this.currentStateSelected)
-    {
+  public stateSelected() {
+    // console.log(this.currentStateSelected);
+    if (this.currentStateSelected) {
       // loop through india data to get details of state selected
-      this.indiaData['regionData'].forEach((state)=>
-      {
-        if(state.region === this.currentStateSelected)
-        {
+      this.indiaData['regionData'].forEach((state) => {
+        if (state.region === this.currentStateSelected) {
           // if match then assigning currently selected state's data to currentStateData property
           this.currentStateData = state;
 
           // update current bar chart
-          this.setStateBarChart(state)
+          this.setStateBarChart(state);
+
+          this.getCurrentStateDistricts(this.currentStateSelected)
         }
       })
     }
+  }
+
+  // get selected state districts
+  public getCurrentStateDistricts(state) {
+
+    this.currentStateDistricts = [];
+    for (let x in this.covidData) {
+      if (x === state) {
+
+        for (let y in this.covidData[x].districtData) {
+          // this.currentStateDistricts.push(y);
+
+          let data =
+          {
+            name: y,
+            value: this.covidData[x].districtData[y]
+          };
+
+          // console.log('data : ',data)
+          this.currentStateDistricts.push(data);
+        }
+      }
+    }
+
+    // select first district of a country as default district selected
+    this.currentDistrictSelected = this.currentStateDistricts[0];
+    this.currentDistrictSelection()
+    this.barChartForDistrict(this.currentDistrictSelected);
+  }
+
+  // fired when district is selected from options
+  public currentDistrictSelection() {
+    // console.log('district : ', this.currentDistrictName);
+    if (this.currentDistrictName) {
+      this.currentStateDistricts.forEach((value) => {
+        if (value.name === this.currentDistrictName) {
+          // console.log('found : ', value);
+          this.currentDistrictSelected = value;
+
+          this.barChartForDistrict(this.currentDistrictSelected)
+        }
+      })
+    }
+  }
+
+  // bar chart for district
+  public barChartForDistrict(district) {
+    // console.log(district);
+    this.barChartDistrict = [
+      { data: [district.value.active], label: 'active' },
+      { data: [district.value.deceased], label: 'deaths' },
+      { data: [district.value.recovered], label: 'recovered' }
+    ]
   }
 
 
